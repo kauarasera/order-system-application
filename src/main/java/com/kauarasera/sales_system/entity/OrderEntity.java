aa733fea08ae7b3921c381fd211a29e64da65aa4
Kauã Rasera <kauarasera@gmail.com>
2025-06-13 18:03:09 +0100

    feat: create OrderEntity with basic value, code and discount attributes

---
+++ src/main/java/com/kauarasera/sales_system/entity/OrderEntity.java
@@ -0,0 +1,41 @@
+package com.kauarasera.sales_system.entity;
+
+public class OrderEntity {
+    private Integer code;
+    private Double basic;
+
+    public OrderEntity() {
+    }
+
+    public OrderEntity(Integer code, Double basic, Double discount) {
+        this.code = code;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    private Double discount;
+}
