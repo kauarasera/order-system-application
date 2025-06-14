dfc37ccebc0772a657eb42b88b15dbc6e87a2f8c
Kauã Rasera <kauarasera@gmail.com>
2025-06-14 08:20:34 +0100

    style: rename variable from discount to shippingFee in ShippingService

--- src/main/java/com/kauarasera/sales_system/services/OrderService.java
+++ src/main/java/com/kauarasera/sales_system/services/OrderService.java
@@ -1,6 +1,12 @@
 package com.kauarasera.sales_system.services;
 
+import com.kauarasera.sales_system.entity.OrderEntity;
+
 public class OrderService {
 
 //injetar ShippingService pois a Order depende dele para o resultado final
+
+    public double total(OrderEntity order) {
+
+    }
 }
