9911a2d021ab2670037ce54a1d7d5057149a18c1
Kauã Rasera <kauarasera@gmail.com>
2025-06-14 10:05:00 +0100

    feat: add simulation to show order total in terminal using CommandLineRunner

--- src/main/java/com/kauarasera/sales_system/OrderSystemApplication.java
+++ src/main/java/com/kauarasera/sales_system/OrderSystemApplication.java
@@ -1,13 +1,28 @@
 package com.kauarasera.sales_system;
 
+import com.kauarasera.sales_system.entity.OrderEntity;
+import com.kauarasera.sales_system.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderSystemApplication {
+public class OrderSystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	@Autowired
+	private OrderService orderService;
+
+	public static void main(String[] args){
 		SpringApplication.run(OrderSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		OrderEntity order = new OrderEntity(2280, 800.0, 10.0);
+		double total = orderService.total(order);
+
+		System.out.println("Order code = " + order.getCode());
+		System.out.println("Total value = " + String.format("%.2f", total));
+	}
 }
