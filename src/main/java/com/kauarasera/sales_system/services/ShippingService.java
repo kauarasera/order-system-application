e8c4e9952c8d8a4969557626979d6957ddc93d11
Kauã Rasera <kauarasera@gmail.com>
2025-06-14 08:17:40 +0100

    style: rename variable from discount to shippingFee in ShippingService

--- src/main/java/com/kauarasera/sales_system/services/ShippingService.java
+++ src/main/java/com/kauarasera/sales_system/services/ShippingService.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShippingService {
     public double shipment(OrderEntity order){
-        double discount = 0.0;
+        double shippingFee;
 
         if (order.getBasic() < 100) {
-            discount = 20.0;
+            shippingFee = 20.0;
         } else if (order.getBasic() < 200) {
-            discount =  12.0;
+            shippingFee =  12.0;
         } else {
-            discount = 0.0;
+            shippingFee = 0.0;
         }
 
-        return discount;
+        return shippingFee;
     }
 }
